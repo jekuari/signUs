8f4eaae18dcb31262f333280f8a14b577b68441e
Ricardo F <ricardo@jekuari.mx>
2023-07-25 17:20:20 -0600

    Switched to intellij

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Simple GUI Test");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(300, 200);
+            frame.setVisible(true);
+        });
     }
 }
